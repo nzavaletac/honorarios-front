53a47805b154d48dafa75090300d531ae80bb900
Noe Zavaleta C <nzavaletac@gmail.com>
2022-10-16 23:47:53 -0500

    Sidebar masomenos terminado. Depende si se me ocurre se le agrega

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,44 @@
+import { Container, Lista, Span, Subtitle } from "./SidebarElements";
+import { FaUsers, FaKey } from "react-icons/fa";
+import { RiMoneyDollarBoxFill, RiLogoutBoxRFill } from "react-icons/ri";
+import { AiFillSetting, AiOutlineArrowRight } from "react-icons/ai";
+
+export const Sidebar = () => {
+  return (
+    <Container>
+      <ul>
+        <Lista>
+          <Span>
+            <FaUsers size="30px" />
+          </Span>
+          <Subtitle>Clientes</Subtitle>
+        </Lista>
+        <Lista>
+          <Span>
+            <FaKey size="30px" />
+          </Span>
+          <Subtitle> Clave Sol</Subtitle>
+        </Lista>
+        <Lista>
+          <Span>
+            <RiMoneyDollarBoxFill size="35px" />
+          </Span>
+          <Subtitle>Honorarios</Subtitle>
+        </Lista>
+        <hr />
+        <Lista>
+          <Span>
+            <AiFillSetting size="35px" />
+          </Span>
+          <Subtitle>Settings</Subtitle>
+        </Lista>
+        <Lista>
+          <Span>
+            <RiLogoutBoxRFill size="35px" />
+          </Span>
+          <Subtitle>Salir</Subtitle>
+        </Lista>
+      </ul>
+    </Container>
+  );
+};
