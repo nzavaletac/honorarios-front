caf4111f57eb34a253acb41f68ba07753d1a9a6a
Noe Zavaleta C <nzavaletac@gmail.com>
2022-11-27 02:09:01 -0500

    Tabala funcional pero falta aún

--- src/components/Tables/ListClient.jsx
+++ src/components/Tables/ListClient.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
 import axios from "axios";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import DataTable from "react-data-table-component";
 
 export const ListClient = () => {
   const [cliente, setCliente] = useState([]);
@@ -14,179 +17,188 @@ export const ListClient = () => {
     }).then(({ data }) => setCliente(data.clientes));
   }, []);
 
+  const customStyles = {
+    rows: {
+      style: {
+        textAlign: "center",
+        fontSize: "13px",
+      },
+    },
+    headCells: {
+      style: {
+        backgroundColor: "#35353f",
+        color: "#fff",
+        fontSize: "15px",
+      },
+    },
+    cells: {
+      style: {},
+    },
+  };
+
+  const columns = [
+    {
+      name: "Ruc",
+      selector: (cliente) => cliente.ruc,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Razon Social",
+      selector: (cliente) => cliente.nombre,
+      sortable: true,
+      center: true,
+      compact: true,
+      sortServer: true,
+      wrap: true,
+      minWidth: "300px",
+    },
+    {
+      name: "Planilla",
+      selector: (cliente) => cliente.planilla,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Régimen",
+      selector: (cliente) => cliente.regimen,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Correo",
+      selector: (cliente) => cliente.email,
+      sortable: true,
+      center: true,
+      compact: true,
+      wrap: true,
+      minWidth: "250px",
+    },
+    {
+      name: "Teléfono",
+      selector: (cliente) => cliente.telefono,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Estado",
+      selector: (cliente) => cliente.estado,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Resp.",
+      selector: (cliente) => cliente.responsable,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Hono.",
+      selector: (cliente) => cliente.honorarios,
+      sortable: true,
+      center: true,
+      compact: true,
+    },
+    {
+      name: "Acciones",
+      selector: () => {
+        return (
+          <>
+            <button style={{ marginRight: "10px" }}>
+              <FaEdit />
+            </button>
+            <button style={{ marginLeft: "10px" }}>
+              <AiFillDelete />
+            </button>
+          </>
+        );
+      },
+      center: true,
+      compact: true,
+    },
+  ];
+
   return (
-    <>
-      <div>
-        <DataTable
-          value={cliente}
-          paginator
-          rows={10}
-          responsiveLayout="scroll"
+    <div>
+      <DataTable
+        title="Clientes"
+        columns={columns}
+        data={cliente}
+        selectableRows
+        fixedHeader
+        fixedHeaderScrollHeight="700px"
+        pagination
+        paginationPerPage="15"
+        // theme="dark"
+        striped
+        highlightOnHover
+        pointerOnHover
+        customStyles={customStyles}
+      />
+
+      {/* <Table striped bordered hover>
+        <thead
+          style={{
+            textAlign: "center",
+            justifyContent: "center",
+            fontSize: "15px",
+            backgroundColor: "#35353f",
+            color: "#fff",
+          }}
         >
-          <Column
-            field="ruc"
-            header="Ruc"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="nombre"
-            header="Nombres y Apellidos o Razon Social"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="planilla"
-            header="Planilla"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="regimen"
-            header="Régimen Tributario"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="email"
-            header="Correo"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="telefono"
-            header="Teléfono"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="estado"
-            header="Estado"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="responsable"
-            header="Responsable"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-          <Column
-            field="honorarios"
-            header="Honorarios"
-            style={{
-              width: "auto",
-              height: "50px",
-              padding: "5px",
-              border: "1px solid gray",
-              textAlign: "center",
-              fontSize: "12px",
-            }}
-            headerStyle={{
-              backgroundColor: "GrayText",
-              textAlign: "center",
-              fontSize: "17px",
-              border: "1px solid white",
-            }}
-          ></Column>
-        </DataTable>
-      </div>
-    </>
+          <tr>
+            <th>#</th>
+            <th>Ruc</th>
+            <th>Razon Social</th>
+            <th>¿Planilla?</th>
+            <th>Correo</th>
+            <th>Teléfono</th>
+            <th>Estado</th>
+            <th>Resp.</th>
+            <th>Hono.</th>
+            <th colSpan={2}>Acciones</th>
+          </tr>
+        </thead>
+        <tbody
+          style={{
+            textAlign: "center",
+            justifyContent: "center",
+            fontSize: "13px",
+          }}
+        >
+          {cliente.map((customer) => {
+            return (
+              <tr>
+                <td>{}</td>
+                <td>{customer.ruc}</td>
+                <td>{customer.nombre}</td>
+                <td>{customer.planilla}</td>
+                <td>{customer.email}</td>
+                <td>{customer.telefono}</td>
+                <td>{customer.estado}</td>
+                <td>{customer.responsable}</td>
+                <td>{customer.honorarios}</td>
+                <td>
+                  <button>
+                    <FaEdit width="20px" color="" />
+                  </button>
+                </td>
+                <td>
+                  <button>
+                    <AiFillDelete width="20px" color="" />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>  */}
+    </div>
   );
 };
